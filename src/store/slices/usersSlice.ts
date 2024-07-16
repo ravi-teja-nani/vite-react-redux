da6643877a8a4ed14b9d4683f6769fdab2321af4
ravitejakorada <tej.r@effibuild.co-at-891376921657>
2024-07-16 19:42:28 +0530

    initial setup

---
+++ src/store/slices/usersSlice.ts
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
+
+export interface UsersSlice {
+  user: {
+    email: string
+    firstName: string
+    lastName: string
+  }
+}
+
+const initialState: UsersSlice = {
+  user: {
+    email: '',
+    firstName: '',
+    lastName: ''
+  }
+}
+
+export const usersSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+  },
+})
+
+export default usersSlice.reducer
