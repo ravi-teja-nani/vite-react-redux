c273d1ab46f1b1d491ff9b63372c1a4a1ec7fb57
ravitejakorada <tej.r@effibuild.co-at-891376921657>
2024-07-22 18:04:07 +0530

    route and auth setup

---
+++ src/lib/api.ts
@@ -0,0 +1,32 @@
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+
+const api: AxiosInstance = axios.create({
+  baseURL: 'https://url.com', 
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+api.interceptors.request.use(
+  (config: AxiosRequestConfig) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response: AxiosResponse) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
